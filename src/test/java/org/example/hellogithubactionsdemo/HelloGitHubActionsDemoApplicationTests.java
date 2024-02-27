dbc6b570b027b1c4e66cfa590805c6f5f2df1224
PaulSebastianKrus <paulsebastiankrus@gmail.com>
2024-02-27 17:44:41 +0100

    Initial commit

---
+++ src/test/java/org/example/hellogithubactionsdemo/HelloGitHubActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hellogithubactionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGitHubActionsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
